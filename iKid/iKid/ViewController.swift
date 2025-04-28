b8b31701acb1962a8df2bb1af272bc15775f254d
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2025-04-28 12:29:06 -0700

    added vcs

---
+++ iKid/iKid/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Bella Gatzemeier on 4/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func Next(_ sender: Any) {
+        
+    }
+    
+    
+}
+
